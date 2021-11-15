fc78931601ea4c83c17e1d5c38df1cdc34698706
Alexandr Makarov <retributioniswar@gmail.com>
2021-11-15 21:19:03 +0700

    temporary remove initializing with configs dict

--- distconfig/confighandler/confighandler.py
+++ distconfig/confighandler/confighandler.py
@@ -1,16 +1,20 @@
-from typing import Dict, Optional
+from typing import Dict
 import os
 
 from .parsers.parser import AbstractConfigParser
 
 
 class ConfigHandler:
-    def __init__(self, parsers: Dict[str, AbstractConfigParser], cfg_dir: Optional[str] = None, configs: Optional[Dict[str, dict]] = None):
-        assert cfg_dir or configs
-
+    def __init__(
+        self,
+        parsers: Dict[str, AbstractConfigParser],
+        cfg_dir: str
+    ):
         self.parsers = parsers
         self.cfg_dir = cfg_dir
-        self.configs: Dict[str, Dict[str, dict]] = {}  # service name : config name: config
+        self.configs: Dict[
+            str, Dict[str, dict]
+        ] = {}  # service name : config name: config
 
         service_names = os.listdir(cfg_dir)
 
@@ -20,7 +24,9 @@ class ConfigHandler:
             config_files = os.listdir(project_path)
 
             for config_name in config_files:
-                self.configs[project_name][config_name] = self.parse_config_file(os.path.join(project_path, config_name))
+                self.configs[project_name][config_name] = self.parse_config_file(
+                    os.path.join(project_path, config_name)
+                )
 
     def parse_config_file(self, path):
         parser = self.select_suitable_parser(path)
@@ -28,12 +34,14 @@ class ConfigHandler:
 
     def select_suitable_parser(self, path):
         _, extension = os.path.splitext(path)
-        
-        assert extension, ValueError(f'No extension in path {path}')
+
+        assert extension, ValueError(f"No extension in path {path}")
 
         extension = extension[1:]  # remove dot
 
         if extension not in self.parsers:
-            raise ValueError(f'No suitable parse for extension {extension} of path {path}')
+            raise ValueError(
+                f"No suitable parse for extension {extension} of path {path}"
+            )
 
         return self.parsers[extension]
