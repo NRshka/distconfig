cc1fd35f12cb2feb9c96c1ca6779e93a9de4bbd5
Alexandr Makarov <retributioniswar@gmail.com>
2021-11-16 15:55:02 +0700

    fix with static method

--- distconfig/confighandler/parsers/jsonparser.py
+++ distconfig/confighandler/parsers/jsonparser.py
@@ -3,7 +3,8 @@ import json
 from .parser import AbstractConfigParser
 
 
-class JSONParser:
+class JSONParser(AbstractConfigParser):
+    @staticmethod
     def parse(path: str) -> dict:
         with open(path, 'r') as file:
             return json.load(file)
